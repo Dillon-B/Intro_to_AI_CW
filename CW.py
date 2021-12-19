cf65faddc8e706a75469346e16e4cfa244fad276
Dillon-B <Dillon.Bhanderi@city.ac.uk>
2021-12-19 18:33:58 +0000

    Update CW.py

--- CW.py
+++ CW.py
@@ -9,7 +9,13 @@ Created on Sat Dec 18 15:46:11 2021
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow.keras.utils
+import io
 
+from tensorflow import keras
+from sklearn import metrics
+from sklearn import preprocessing
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -17,3 +23,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import KFold
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import EarlyStopping  
+from tensorflow.keras.layers import Dropout 
