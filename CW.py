4803d75223cff5d1935418d98a8bfbddd1649a4b
Dillon-B <Dillon.Bhanderi@city.ac.uk>
2021-12-19 11:16:13 +0000

    imports

--- CW.py
+++ CW.py
@@ -8,3 +8,12 @@ Created on Sat Dec 18 15:46:11 2021
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
