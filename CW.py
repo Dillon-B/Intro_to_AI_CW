2936fcffbc7da3129e3fe823a802d446b0db391a
rae2001 <raediftikhar2001@gmail.com>
2021-12-26 22:16:08 +0000

    Update CW.py

--- CW.py
+++ CW.py
@@ -18,6 +18,9 @@ import os
 from tensorflow import keras
 from sklearn import metrics
 from sklearn import preprocessing
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -31,13 +34,14 @@ from tensorflow.keras import regularizers
 from tensorflow.keras.callbacks import EarlyStopping  
 from tensorflow.keras.layers import Dropout 
 
-fortune = pd.read_csv('datasetF1000.csv', nrows= 1000)
+fortune = pd.read_csv('datasetF1000.csv')
 fortune
 
-X = fortune[['market value']]
-y = fortune[['Revenue']]
+X = fortune.drop(['market value'], axis=1)
+Y = fortune[['market value']]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=(45))
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
 training_data, testing_data = train_test_split(fortune, test_size= 0.25, random_state=(30))
 
@@ -45,15 +49,25 @@ print(f"No. of training examples: {training_data.shape[0]}")
 print(f"No. of testing examples: {testing_data.shape[0]}")
 print(f"No. of training examples: {X_train.shape[0]}")
 print(f"No. of testing examples: {X_test.shape[0]}")
-print(f"No. of training examples: {y_train.shape[0]}")
-print(f"No. of testing examples: {y_test.shape[0]}")
+print(f"No. of training examples: {Y_train.shape[0]}")
+print(f"No. of testing examples: {Y_test.shape[0]}")
 print(training_data.shape)
 print(testing_data.shape)
 print(X_train.shape)
 print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
+print(Y_train.shape)
+print(Y_test.shape)
 
-perceptronFortune = Perceptron(max_iter= 60, tol=(0.001), eta0=1)
-perceptronFortune.fit(X_train,y_train)
-pred_perceptron = perceptronFortune.predict(X_train)
+#perceptronFortune = Perceptron(max_iter= 60, tol=(0.001), eta0=1)
+#perceptronFortune.fit(X_train,y_train)
+#pred_perceptron = perceptronFortune.predict(X_train)
+
+trainingModel = linear_model.LinearRegression()
+
+trainingModel.fit(X_train, Y_train)
+
+LinearRegression(copy_X=True, fit_itercept=True, n_jobs=None, normalize=False)
+
+Y_pred = trainingModel.predict(X_test)
+
+
